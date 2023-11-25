01ce372b812172c1e39f0a6470ac3086e66c1e1e
aljo <johansson_a95@hotmail.com>
2023-11-25 22:17:27 +0100

    initial components

---
+++ src/components/Kurser.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '../App.css'
+import NavBar from '../NavBar.jsx';
+
+function Kurser() {
+  
+    return (
+      <>
+        <NavBar />
+        <p>KURSER HÄR!!!!!!!!!!!!!!!!!!!!!!</p>
+
+      </>
+    )
+  }
+  
+  export default Kurser
---
+++ src/components/Rasthage.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '../App.css'
+import NavBar from '../NavBar.jsx';
+
+function Rasthage() {
+  
+    return (
+      <>
+        <NavBar />
+        <p>RASTHAGE HÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄR!</p>
+
+      </>
+    )
+  }
+  
+  export default Rasthage
